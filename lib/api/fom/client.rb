5b53049ce1cd8ba985c46e66eca2f0e36a19ef80
Metin Yorulmaz <metin@me.com>
2014-08-21 14:57:04 -0400

    Initial commit

---
+++ lib/api/fom/client.rb
@@ -0,0 +1,9 @@
+require "api/fom/client/version"
+
+module API
+  module FOM
+    module Client
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/api/fom/client/version.rb
@@ -0,0 +1,7 @@
+module API
+  module FOM
+    module Client
+      VERSION = "0.0.1"
+    end
+  end
+end
